8cdcc901ec30c91af1fad28f2beeff2b11d980c6
Tien Phan <tienphan1989@gmail.com>
2020-07-31 00:27:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(string=`*`) {
+    return (str='special') => {
+        return `You are ${string}${str}${string}!`;
+    }
+}
+
+const Calculator = {
+    add: (a,b) => {
+        return a + b;
+    }, 
+    subtract: (a,b) => {
+        return a - b;
+    },
+    multiply: (a,b) => {
+        return a * b;
+    },
+    divide:(a,b) => {
+        return a / b;
+    }
+}
+
+function actionApplyer(int, array) {
+    if(array.length === 0){
+        return int;
+    }
+    return (((int * 2) + 1000) % 7)
+
+}
